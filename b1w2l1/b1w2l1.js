d1c0367ac6b605c6f26b80969ab343f0caeeef6f
Merel <99059127@mydavinci.nl>
2019-09-09 14:12:12 +0200

    .,

---
+++ b1w2l1/b1w2l1.js
@@ -0,0 +1,5 @@
+window.alert("Je moet de volgende 3 vragen beantwoorden");
+var name = prompt("Wat is je voornaam?","Merel");
+var last name = prompt("Wat is je achternaam?","van de Bree");
+var age = prompt("Hoe oud ben je?","18");
+
