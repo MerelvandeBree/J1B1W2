cca8ddb179e79acfb4edbdf7e2033f038733e873
Merel <99059127@mydavinci.nl>
2019-09-10 09:58:09 +0200

    Lab 2 afgerond

--- b1w2l1/b1w2l1.js
+++ b1w2l1/b1w2l1.js
@@ -1,9 +1,10 @@
-// begin hier je JavaScript commandos
 
-window.alert("je moet 3 vragen beantwoorden")
-var name = prompt("Wat is je naam?", "")
-var surname = prompt("Wat is je achernaam?", "")
-var age = prompt("Hoe oud ben je?", "")
-document.write(name + "h" + surname)
-document.write("<br/>")
-document.write("" + age + "")
+window.alert("Je moet 3 vragen beantwoorden")
+
+var name = prompt("Wat is je naam", " ")
+var surname = prompt("Wat is je achternaam", " ")
+var age = prompt("Hoe oud ben je", " ")
+
+document.write('<br/>' + "Naam : " + prompt("Wat is je naam?") );
+document.write('<br/>' + "Achternaam : " + prompt("Wat is je achternaam?") )
+document.write('<br/>' + "Leeftijd : " + prompt("Hoe oud ben je?") )
